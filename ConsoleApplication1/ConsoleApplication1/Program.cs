56b939382e9a82c2e1778b534b1b1cd73a033775
ramlak98 <ramlak98@gmail.com>
2016-04-29 17:53:27 +0800

    Initial submission of console application

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "c:\\temp\\myfile.bmp";
+            string relativeFile = ".\\woohoo\\temp.bmp";
+            string addressName = @"http://www.google.com/blahblah.html";
+
+            Uri uriFile = new Uri(fileName);
+            Uri uriRelative = new Uri(uriFile, relativeFile);
+            Uri uriAddress = new Uri(addressName);
+
+            Console.WriteLine(uriFile.ToString());
+            Console.WriteLine(uriRelative.ToString());
+            Console.WriteLine(uriAddress.ToString());
+        }
+    }
+}
